3a6d2f80c098daf509cac0f9b489c31f2d7614f2
Henrik Lábadi <hlabadi@hotmail.com>
2014-07-16 19:15:06 +0200

    AboutPage complete

--- MapprDrone.WP8/AboutPage.xaml.cs
+++ MapprDrone.WP8/AboutPage.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using Microsoft.Phone.Tasks;
 
 namespace MapprDrone.WP8
 {
@@ -15,6 +16,29 @@ namespace MapprDrone.WP8
         public AboutPage()
         {
             InitializeComponent();
+        }
+
+        //rate
+        private void Grid_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            MarketplaceReviewTask mrt = new MarketplaceReviewTask();
+            mrt.Show();
+        }
+
+        // twitter
+        private void Grid_Tap_1(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            WebBrowserTask wbt = new WebBrowserTask();
+            wbt.Uri = new Uri("https://twitter.com/hlabadi");
+            wbt.Show();
+        }
+
+        // github
+        private void Grid_Tap_2(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            WebBrowserTask wbt = new WebBrowserTask();
+            wbt.Uri = new Uri("https://github.com/hlabadi/MapprDrone");
+            wbt.Show();
         }
     }
 }
